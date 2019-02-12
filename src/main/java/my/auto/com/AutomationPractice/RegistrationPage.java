7fba32743d8b1f1c6896955a9cc9b5ceac4404d9
devayanip <pawardevayani20@gmail.com>
2019-02-12 08:50:50 +0530

    Nick

--- src/main/java/my/auto/com/AutomationPractice/RegistrationPage.java
+++ src/main/java/my/auto/com/AutomationPractice/RegistrationPage.java
@@ -38,12 +38,12 @@ public class RegistrationPage {
 		
 		genderRadioButton.click();
 		
-		FirstnameTextBox.sendKeys("Damini");
+		FirstnameTextBox.sendKeys("Nick");
 
-		LastnameTextBox.sendKeys("Deokar");
+		LastnameTextBox.sendKeys("Jonas");
 
 	
-		passwordTextBox.sendKeys("damini");
+		passwordTextBox.sendKeys("nick");
 
 		Select DayButton = new Select(selectDay);
 		DayButton.selectByIndex(23);
